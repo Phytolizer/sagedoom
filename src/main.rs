c3bd10c027975a39d2776cac4baba5acbe8cc8db
Kyle Coffey <me@phytolizer.dev>
2022-08-07 15:56:32 -0500

    load IWAD properly

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,61 @@
 // Needed for `box State::new()`.
 #![feature(box_syntax)]
 
+use std::env;
+use std::path::Path;
+use std::path::PathBuf;
+
+use state::GameMode;
 use state::State;
 
 mod state;
 mod wad;
 
+fn add_file(state: &mut State, file: impl AsRef<Path>) {
+    let file = file.as_ref();
+    state.wad_files.push(file.to_path_buf());
+}
+
+fn identify_version(state: &mut State) {
+    let doom_wad_dir = PathBuf::from(env::var("DOOMWADDIR").unwrap_or_else(|_| ".".to_string()));
+    let doom2_wad = doom_wad_dir.join("doom2.wad");
+    let doomu_wad = doom_wad_dir.join("doomu.wad");
+    let doom_wad = doom_wad_dir.join("doom.wad");
+    let doom1_wad = doom_wad_dir.join("doom1.wad");
+    let plutonia_wad = doom_wad_dir.join("plutonia.wad");
+    let tnt_wad = doom_wad_dir.join("tnt.wad");
+
+    if doom2_wad.exists() {
+        state.game_mode = GameMode::Commercial;
+        add_file(state, &doom2_wad);
+    } else if plutonia_wad.exists() {
+        state.game_mode = GameMode::Commercial;
+        add_file(state, &plutonia_wad);
+    } else if doomu_wad.exists() {
+        state.game_mode = GameMode::Retail;
+        add_file(state, &doomu_wad);
+    } else if doom_wad.exists() {
+        state.game_mode = GameMode::Registered;
+        add_file(state, &doom_wad);
+    } else if doom1_wad.exists() {
+        state.game_mode = GameMode::Shareware;
+        add_file(state, &doom1_wad);
+    } else if tnt_wad.exists() {
+        state.game_mode = GameMode::Shareware;
+        add_file(state, &tnt_wad);
+    } else {
+        println!("Game mode indeterminate.");
+        state.game_mode = GameMode::Undetermined;
+    }
+}
+
 fn main() {
     let mut state = box State::new();
 
-    wad::init(&mut state, vec![]);
+    identify_version(&mut state);
+
+    let wad_files = state.wad_files.clone();
+    wad::init(&mut state, wad_files);
 
     dbg!(state);
 }
--- src/state.rs
+++ src/state.rs
@@ -1,12 +1,27 @@
+use std::path::PathBuf;
+
+#[derive(Debug)]
+pub(crate) enum GameMode {
+    Commercial,
+    Retail,
+    Registered,
+    Shareware,
+    Undetermined,
+}
+
 #[derive(Debug)]
 pub(crate) struct State {
     pub(crate) wad: crate::wad::State,
+    pub(crate) game_mode: GameMode,
+    pub(crate) wad_files: Vec<PathBuf>,
 }
 
 impl State {
     pub(crate) fn new() -> Self {
         Self {
             wad: crate::wad::State::new(),
+            game_mode: GameMode::Undetermined,
+            wad_files: Vec::new(),
         }
     }
 }
